0551fd45936b7547d2ec2dff4e01ab8ece5181a4
Burkin Nikita <92577296+ChackChock@users.noreply.github.com>
2024-12-29 14:03:25 +0300

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
  
 public class Player : MonoBehaviour
 {
-    //Здоровье игрока
+    //Текущее здоровье игрока
     private int health = 10;
  
     //Число собранных монет
@@ -15,11 +16,29 @@ public class Player : MonoBehaviour
     public Transform attackPoint;
  
  
-    //Метод, понижающий здоровье игрока
+   //Компонент, отвечающий за проигрывание звуков
+    public AudioSource audioSource;
+ 
+    //Звуковой файл, содержащий звуковой эффект нанесения урона
+    public AudioClip damageSound;
+ 
+    //Метод, обрабатывающий нанесённый урон
     public void TakeDamage(int damage)
     {
         health -= damage;
-        print("Здоровье игрока: " + health);
+ 
+        //Если здоровье ещё есть, то проигрывается звук нанесения урона
+        if(health > 0)
+        {
+            audioSource.PlayOneShot(damageSound);
+            //print("Здоровье игрока: " + health);
+        }
+        //Если здоровья нет, то перезапускается текущая сцена
+        else
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
     }
  
     //Метод, увеличивающий число монеток
